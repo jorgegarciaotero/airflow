04e190eae8c1ae13cb18c0120aaa6ebf01666d9b
jgotero1 <jorgegarciaotero@gmail.com>
2022-12-03 08:54:29 +0000

    v2. airflow examples

--- dags/01_basic_dag.py
+++ dags/01_basic_dag.py
@@ -10,7 +10,25 @@ from airflow.utils.dates import days_ago
 
 default_args = {
     'owner':'Airflow',
-    'start_date': datetime(2022,11,18, 19,50,00)
+    'start_date': datetime(2022,11,18, 19,50,00),
+    'depends_on_past': False,
+    'email': ['jorgegarciaotero@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
 }
 
 
@@ -18,7 +36,7 @@ def helloWorldLoop():
     for palabra in ['hello','world']:
         print(palabra)
 
-with DAG(dag_id='basic_dag',
+with DAG(dag_id='01_basic_dag',
     default_args=default_args,
     schedule_interval='@once') as dag:
     
